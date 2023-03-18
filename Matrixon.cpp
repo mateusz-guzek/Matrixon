d3f3d3a9e2fd2eda48a39722fb5e95ae5416f992
Matixon72 <matixon72@gmail.com>
2023-03-18 22:24:50 +0100

    font i plany na spadanie liter

--- Matrixon.cpp
+++ Matrixon.cpp
@@ -1,14 +1,17 @@
 ﻿#include <SFML/Graphics.hpp>
 #include <vector>
 #include <random>
+#include <stdlib.h>
+#include <time.h>
 
-std::mt19937 engine;
 
 char randomCharFromString(std::string word) {
+    static std::mt19937 engine;
     std::uniform_int_distribution<int> dist(0, (int)word.length());
     return word[dist(engine)];
 }
-static char randomInt(int min, int max) {
+int randomInt(int min, int max) {
+    static std::mt19937 engine;
     std::uniform_int_distribution<int> dist(min, max);
     return dist(engine);
 }
@@ -19,7 +22,7 @@ private:
     sf::RenderWindow window;
     sf::Font mono_font;
     sf::Text character;
-    std::string characters = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM<>?/;:[{}])(&*+=";
+    std::string characters = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM<>?/[{}])(&*+=";
     std::vector<int> drops;
     float row_width;
     bool running;
@@ -58,20 +61,23 @@ private:
     }
 
     void draw() {
-        //window.clear();
+        window.clear();
         //***
         for (int i = 0; i < drops.size(); i++)
         {
-            int x = randomInt(0, window.getSize().x);
             int y = randomInt(0, window.getSize().y);
-            printf("%d,%d \n", x, y);
+            //printf("%d,%d \n", x, y);
             character.setString(randomCharFromString(characters));
             character.setCharacterSize(randomInt(10, row_width));
-            character.setPosition(x, y);
+            character.setPosition(i*row_width+4, drops[i]);
             window.draw(character);
+            /*
+            drops[i]+=5;
+            if (drops[i] > window.getSize().y) drops[i] = randomInt(-40, -20);
+            printf("%d , %d \n", i, drops[i]);
+            */
         }
 
-        //window.draw(sf::CircleShape(100.f));
 
         //***
         window.display();
@@ -83,12 +89,12 @@ private:
     };
 
 public:
-    Matrix(int width, int height) {
+    Matrix(int width, int height, float colnum) {
         window.create(sf::VideoMode(width, height, 32), "Matrix", sf::Style::Close);
-        window.setFramerateLimit(1);
-        row_width = (float)window.getSize().x / 30.f;
-        mono_font.loadFromFile("Monocraft.ttf");
-        for (size_t i = 0; i < 30; i++) { drops.push_back(0); }
+        window.setFramerateLimit(20);
+        row_width = (float)window.getSize().x / colnum;
+        if (mono_font.loadFromFile("Monocraft.ttf")) printf("udalo si");
+        drops = std::vector<int>(int(colnum), 0);
         character.setFont(mono_font);
         character.setFillColor(sf::Color::Green);
         running = true;
@@ -113,7 +119,7 @@ public:
 
 int main()
 {
-    Matrix matrix(600, 600);
+    Matrix matrix(600, 600, 40.f);
     matrix.run();
 
     return 0;
