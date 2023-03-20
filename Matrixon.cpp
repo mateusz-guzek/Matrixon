f1187984d75e63adea32762fddbf39038c3e5503
Matixon72 <matixon72@gmail.com>
2023-03-20 21:16:05 +0100

    start of config reader for resolution

--- Matrixon.cpp
+++ Matrixon.cpp
@@ -3,6 +3,8 @@
 #include <random>
 #include <stdlib.h>
 #include <time.h>
+#include <Windows.h>
+#include <fstream>
 
 
 char randomCharFromString(std::string word) {
@@ -16,6 +18,17 @@ int randomInt(int min, int max) {
     return dist(engine);
 }
 
+void changeConsole(bool f)
+{
+    if (!f) {
+        ShowWindow(GetConsoleWindow(), SW_HIDE);
+    }
+    else if (f) {
+        ShowWindow(GetConsoleWindow(), SW_SHOW);
+    }
+
+}
+
 
 class Matrix {
 private:
@@ -25,7 +38,8 @@ private:
     std::string characters = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM<>?/[{}])(&*+=";
     std::vector<int> drops;
     sf::RectangleShape background;
-    float row_width;
+    sf::RenderTexture brak_pomyslu_na_nazwe_ale_to_jest_bufor_tla;
+    float col_width;
     bool running;
 
 
@@ -52,6 +66,15 @@ private:
                 case sf::Keyboard::Escape:
                     running = false;
 
+                case sf::Keyboard::R:
+                    if (IsWindowVisible(GetConsoleWindow())) {
+                        window.requestFocus();
+                    }
+                    else {
+                        changeConsole(true);
+                    }
+                    
+
                 default:
                     break;
                 }
@@ -62,45 +85,58 @@ private:
     }
 
     void draw() {
-        //window.clear();
-        window.draw(background);
-        //***
+        window.clear();
+
+        brak_pomyslu_na_nazwe_ale_to_jest_bufor_tla.draw(background);
         for (int i = 0; i < drops.size(); i++)
         {
             character.setString(randomCharFromString(characters));
-            character.setCharacterSize(randomInt(10, row_width));
-            character.setPosition(i*row_width+4, drops[i]);
-            window.draw(character);
+            character.setPosition(i*col_width+1, drops[i]);
+            character.setFillColor(sf::Color(0, 255, 0, 255));
+            brak_pomyslu_na_nazwe_ale_to_jest_bufor_tla.draw(character);
             
-            drops[i]+=8;
-            if (drops[i] > window.getSize().y) {
+            drops[i]+=character.getCharacterSize();
+
+            if (drops[i] > brak_pomyslu_na_nazwe_ale_to_jest_bufor_tla.getSize().y) {
                 drops[i] = randomInt(0, 400)*-1;
             }
-            printf("%d , %d \n", window.getSize().y, drops[i]);
+            printf("%d , %d \n", window.getSize().x, drops.size());
         }
+
+        sf::Sprite bufor_tla_ale_sprite_bo_inaczej_nie_dziala(brak_pomyslu_na_nazwe_ale_to_jest_bufor_tla.getTexture());
+        bufor_tla_ale_sprite_bo_inaczej_nie_dziala.rotate(180.f);
+        bufor_tla_ale_sprite_bo_inaczej_nie_dziala.move(window.getSize().x, window.getSize().y);
+
+        window.draw(bufor_tla_ale_sprite_bo_inaczej_nie_dziala);
 
 
         //***
         window.display();
     };
     void update() {
+        
+        draw();
 
         if (!running)
             window.close();
     };
 
 public:
-    Matrix(int width, int height, float colnum) {
-        window.create(sf::VideoMode(width, height, 32), "Matrix", sf::Style::Close);
-        window.setFramerateLimit(60);
-        row_width = (float)window.getSize().x / colnum;
-        if (mono_font.loadFromFile("Monocraft.ttf")) printf("udalo si");
-        drops = std::vector<int>(int(colnum), 600);
+    Matrix(int width, int height) {
+        window.create(sf::VideoMode(width, height, 32), "Matrixon", sf::Style::Close);
+        window.setFramerateLimit(20);
+        col_width = 20;
+        if (mono_font.loadFromFile("Monocraft.ttf")) printf("mom spaghetti");
+        drops = std::vector<int>(width/col_width, 0);
         character.setFont(mono_font);
         character.setFillColor(sf::Color::Green);
+        character.setStyle(sf::Text::Bold);
+        character.setCharacterSize(col_width);
         background.setSize(sf::Vector2f(window.getSize().x, window.getSize().y));
         background.setFillColor(sf::Color(0, 0, 0, 40));
         background.setPosition(0,0);
+        brak_pomyslu_na_nazwe_ale_to_jest_bufor_tla.create(width, height);
+        brak_pomyslu_na_nazwe_ale_to_jest_bufor_tla.setSmooth(true);
         running = true;
 
     };
@@ -111,7 +147,6 @@ public:
         {
             events();
             update();
-            draw();
         }
     };
 
@@ -123,7 +158,8 @@ public:
 
 int main()
 {
-    Matrix matrix(600, 600, 40.f);
+    changeConsole(false);
+    Matrix matrix(800, 900);
     matrix.run();
 
     return 0;
